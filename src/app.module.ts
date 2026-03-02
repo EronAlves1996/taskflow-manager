32db076f1ade7056d6fe16bfe0c32bc80794ec20
Eron Alves <eron.alves@rocketmail.com>
2026-03-02 14:15:04 -0300

    feat: scaffold task services and controlers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './env.validation';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate,
     }),
+    TaskModule,
   ],
   controllers: [AppController],
   providers: [AppService],
