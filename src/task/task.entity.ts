03cd1aa24898ba5db5166f6833d2ca4acb4fd41a
Eron Alves <eron.alves@rocketmail.com>
2026-03-02 14:54:00 -0300

    feat: create task entity

---
+++ src/task/task.entity.ts
@@ -0,0 +1,39 @@
+import {
+  Priority,
+  type PriorityEnum,
+  TaskStatus,
+  type TaskStatusEnum,
+} from 'src/enums';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 100, type: 'string' })
+  title: string;
+
+  @Column({ type: 'enum', enum: TaskStatus, enumName: 'task_status' })
+  status: TaskStatusEnum;
+
+  @Column({ type: 'enum', enum: Priority, enumName: 'priority' })
+  priority: PriorityEnum;
+
+  @CreateDateColumn({
+    name: 'created_at',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    nullable: true,
+  })
+  updatedAt?: Date;
+}
